97b4d41055eaf649059e86aa0eb64776de1cfc64
shotaro.takimoto <>
2022-02-23 10:31:22 +0900

    初回登録

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      個人うぃき
+      やること
+      ・lintを導入する
+      ・prettierを導入する
+      ・tsconfigを導入する
+      ・ドメインを取得する
+      ・ドメインにサイトを公開する
+      ・
+    </div>
+  );
+}
+
+export default App;

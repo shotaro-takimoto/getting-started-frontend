db2621d3a1b8e98ff11deda892542e12f567e8f2
shotaro.takimoto <>
2022-02-27 10:42:59 +0900

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <div>
-      個人うぃき
-      やること
-      ・lintを導入する
-      ・prettierを導入する
-      ・tsconfigを導入する
-      ・ドメインを取得する
-      ・ドメインにサイトを公開する
-      ・
+      個人うぃき やること ・lintを導入する ・prettierを導入する
+      ・tsconfigを導入する ・ドメインを取得する ・ドメインにサイトを公開する ・
     </div>
   );
 }
